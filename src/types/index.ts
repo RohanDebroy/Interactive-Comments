db731aa54219f43b933ca382d6bfcf5afbf3e795
Rohan Debroy <debroy.debroy2017@gmail.com>
2023-03-31 00:44:36 +0530

    Moved assets to public folder and added types for data

---
+++ src/types/index.ts
@@ -0,0 +1,44 @@
+export interface IRoot {
+  currentUser: ICurrentUser;
+  comments: Comment[];
+}
+
+export interface ICurrentUser {
+  image: IImage;
+  username: string;
+}
+
+export interface IImage {
+  png: string;
+  webp: string;
+}
+
+export interface IComment {
+  id: number;
+  content: string;
+  createdAt: string;
+  score: number;
+  user: IUser;
+  replyingTo?: string;
+  replies?: IComment[];
+}
+
+export interface IUser {
+  image: IImage2;
+  username: string;
+}
+
+export interface IImage2 {
+  png: string;
+  webp: string;
+}
+
+export interface IUser2 {
+  image: IImage3;
+  username: string;
+}
+
+export interface IImage3 {
+  png: string;
+  webp: string;
+}
